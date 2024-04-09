0ba83f50357932eac8fd3af0a0491bc83ac82ba8
changukLee <ckddnr5527@gmail.com>
2024-04-09 14:43:59 +0900

    feat : CH01.변수 update

---
+++ 01.변수/변수.kts
@@ -0,0 +1,16 @@
+// 1. var 변수 선언하기
+var changeYes = 100
+// var change_yes // 오류남, 선언과 동시에 초기화를 시켜줘야 한다
+println(changeYes)
+
+changeYes = 200
+println(changeYes)
+
+// 2. val 변수 선언하기
+val CHANGE_NO = 100
+println(CHANGE_NO)
+//CHANGE_NO = 200 // 오류남, val은 변경불가
+
+val SCHOOL_NAME = "대한민국 학교"
+println(SCHOOL_NAME)
+
