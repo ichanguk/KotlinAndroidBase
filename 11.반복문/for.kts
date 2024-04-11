47f16d8e25ac79a01db785271a11efec9030ed75
changukLee <ckddnr5527@gmail.com>
2024-04-11 18:47:31 +0900

    feat: CH11.반복문(for) update

---
+++ 11.반복문/for.kts
@@ -0,0 +1,35 @@
+// range
+for (value in 0..10) {
+    println("반복")
+}
+
+// progression + step
+for (value in 0..5 step 2) {
+    println("반복 1")
+}
+
+// progression + downTo
+for (value in 5 downTo 0) {
+    println("반복 2")
+}
+
+val numbers = listOf<Int>(5, 6, 7, 8)
+for (number in numbers) {
+    println(number)
+}
+for (i in 0..numbers.size) {
+    println("반복3")
+}
+
+for (i in 0 until numbers.size) {
+    println("반복4")
+}
+
+for ((index, number) in numbers.withIndex()) {
+    println("" + index + "  |  " + number)
+}
+
+// foreach
+numbers.forEach {number ->
+    println(number)
+}
